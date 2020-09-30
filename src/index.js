f8471e9449380ec00fc5a0dc321e2dead98cb53e
Dzima <d.propolsky@gmail.com>
2020-09-30 13:10:53 +0300

    feat: add solution of max, min, avg task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array.length === 0) {
+        return 0;
+    }
+    let arrayOfNumbers = [];
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] === 'number') {
+            arrayOfNumbers.push(array[i]); 
+        }; 
+    };
+    return Math.min(...arrayOfNumbers);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array.length === 0) {
+        return 0;
+    }
+    let arrayOfNumbers = [];
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] === 'number') {
+            arrayOfNumbers.push(array[i]); 
+        }; 
+    };
+    return Math.max(...arrayOfNumbers);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array.length === 0) {
+        return 0;
+    }
+    let arrayOfNumbers = 0;
+    for (let i = 0; i < array.length; i++){
+        if (typeof array[i] === 'number') {
+            arrayOfNumbers += array[i]; 
+        }; 
+    };
+    return arrayOfNumbers/array.length;
 }
